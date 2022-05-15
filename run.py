67f3ba98de6e3cba3b2558e59e88ce98d5eb37d4
Ricardo Saucedo <ricardoarielsaucedo@gmail.com>
2022-05-15 16:07:10 -0300

    changed execution to best practice

--- run.py
+++ run.py
@@ -2,27 +2,30 @@
 from user import User
 from item import Item
 
+def main():
+    menu = {'1': "New User.", '2': "Delete User.", '3': "Find User", '4': "Exit"}
+    while True:
+        opciones = menu.keys()
+        opciones.sort()
+        for entrada in opciones:
+          print("entrada", menu[entrada])
 
-menu = {'1': "New User.", '2': "Delete User.", '3': "Find User", '4': "Exit"}
-while True:
-    opciones = menu.keys()
-    opciones.sort()
-    for entrada in opciones:
-      print("entrada", menu[entrada])
+        selection=input("Choose an option:")
+        if selection =='1':
+          users = {}
+          name=input("Welcome, What username will you use?:")
+          users[name]=User(name)
+          users[name].new_user()
 
-    selection=input("Choose an option:")
-    if selection =='1':
-      users = {}
-      name=input("Welcome, What username will you use?:")
-      users[name]=User(name)
-      users[name].new_user()
+        elif selection == '2':
+          #delete_user
+         elif selection == '3':
+          #eleccion_usuario=input("Ingrese su nombre de usuario:")
+          #user_menu(eleccion_usuario)
+        elif selection == '4':
+          break
+        else:
+          print ("Opcion desconocida")
 
-    elif selection == '2':
-      #delete_user
-     elif selection == '3':
-      #eleccion_usuario=input("Ingrese su nombre de usuario:")
-      #user_menu(eleccion_usuario)
-    elif selection == '4':
-      break
-    else:
-      print ("Opcion desconocida") 
+if __name__ == "__main__"
+    main()
